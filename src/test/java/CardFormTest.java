3aaf314668efb026bfdb4878b52d35f8a76a361c
Ilya Pliev <straiff20@gmail.com>
2020-01-30 12:44:09 +0300

    Fix typos

--- src/test/java/CardFormTest.java
+++ src/test/java/CardFormTest.java
@@ -18,9 +18,9 @@ public class CardFormTest {
         static void setUpAll() {
 
             if ((System.getProperty("os.name")).contains("mac")) {
-                System.setProperty("webdriver.chrome.driver", "Browser_plugins/chrome/mac/chromedriver";
+                System.setProperty("webdriver.chrome.driver", "Browser_plugins/chrome/mac/chromedriver");
             } else if ((System.getProperty("os.name")).contains("linux")) {
-                System.setProperty("webdriver.chrome.driver", "Browser_plugins/chrome/linux/chromedriver";
+                System.setProperty("webdriver.chrome.driver", "Browser_plugins/chrome/linux/chromedriver");
             } else {
                 System.setProperty("webdriver.chrome.driver", "Browser_plugins/chrome/windows/chromedriver.exe");
             }
@@ -41,22 +41,18 @@ public class CardFormTest {
         void cardFormLHappyTest() throws InterruptedException {
             //  правильно заполняем форму
             driver.get("http://localhost:7777");
-            Thread.sleep(2000);
             String title = driver.findElement(By.cssSelector(".heading_theme_alfa-on-white")).getText();
             assertEquals("Заявка на дебетовую карту", title.trim());  //  Заголовок виден и правильно написан
 
             driver.findElement(By.cssSelector("input.input__control[type=text]")).sendKeys("Сергей");
             driver.findElement(By.cssSelector("input.input__control[type=tel]")).sendKeys("+79991112233");
             driver.findElement(By.cssSelector("form span.checkbox__box")).click();
-            Thread.sleep(1000);
 
             driver.findElement(By.className("button__text")).click();
-            Thread.sleep(2000);
 
             driver.findElement(By.className("Success_successBlock__2L3Cw")).isDisplayed();
             String successText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
             assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", successText.trim());
-            Thread.sleep(1000);
 
         }
 
@@ -80,7 +76,6 @@ public class CardFormTest {
             String errorText = driver.findElement(By.cssSelector("div:nth-child(1) > span > span > span.input__sub")).getText();
 
             assertEquals(nameError, errorText.trim());
-            Thread.sleep(1000);
         }
 
         @ParameterizedTest
@@ -102,7 +97,6 @@ public class CardFormTest {
             String errorText = driver.findElement(By.cssSelector("div:nth-child(2) > span > span > span.input__sub")).getText();
 
             assertEquals(phoneError, errorText.trim());
-            Thread.sleep(1000);
         }
     }
 }
